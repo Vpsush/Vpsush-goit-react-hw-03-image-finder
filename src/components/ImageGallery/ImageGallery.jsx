df696a6e05ed149b1fc30ec808f565d9d6a88314
Vpsush <vpsush2023@gmail.com>
2023-10-21 23:03:59 +0300

    2

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import css from './ImageGallery.module.css';
+
+const ImageGallery = () => {
+  return (
+    <ul className={css.gallery}>
+      <li></li>
+    </ul>
+  );
+};
+
+export default ImageGallery;

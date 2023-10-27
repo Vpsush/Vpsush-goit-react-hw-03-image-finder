cf5182f3115d6ba0aae7137151344eb84b848d1f
Vpsush <vpsush2023@gmail.com>
2023-10-28 00:28:13 +0300

    4

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -36,6 +36,20 @@ import React, { Component } from 'react';
 import css from './Modal.module.css';
 
 export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
   render() {
     return (
       <div className={css.Overlay} onClick={this.props.closeModal}>

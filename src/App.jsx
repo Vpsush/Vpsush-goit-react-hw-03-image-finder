84ccf4e59979533b090a9f1dc8b60b1f2fdc1789
Vpsush <vpsush2023@gmail.com>
2023-10-29 16:49:20 +0200

    555

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,162 @@
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import css from 'App.module.css';
+// import Searchbar from './components/Searchbar/Searchbar';
+// import ImageGallery from './components/ImageGallery/ImageGallery';
+// import Loader from './components/Loader/Loader';
+// import Modal from './components/Modal/Modal';
+// import Button from './components/Button/Button';
+// import ErrorCard from './components/ErrorCard';
+// // import fetchImages from './services/fetchImages';
+
+// const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// class App extends Component {
+//   state = {
+//     images: [],
+//     isLoading: false,
+//     error: '',
+//     showModal: false,
+//     query: '',
+//     page: 1,
+//     // status: 'idle',
+//     units: null,
+//     hasMoreImages: true,
+//     // hits: 1,
+//   };
+
+//   fetchImages = async (query, page) => {
+//     try {
+//       this.setState({
+//         isLoading: true,
+//       });
+//       const { data } = await axios.get(
+//         `/?q=${query}&key=${API_KEY}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+//       );
+
+//       if (data.hits.length === 0) {
+//         this.setState({
+//           error: `No results found for "${query}"`,
+//           // status: 'rejected',
+//           hasMoreImages: false,
+//         });
+//         return;
+//       }
+
+//       this.setState(prevState => ({
+//         images: [...prevState.images, ...data.hits],
+//         error: null,
+//       }));
+
+//       // this.steState(prevState => ({
+//       //   images: [...prevState.images, ...data.hits],
+//       //   loadMore: this.state.page < Math.ceil(data.totalHits / 12),
+//       // }));
+
+//       // if (data.status === 'ok') this.setState({ units: data.unit });
+//       // else return Promise.reject(data.message);
+
+//       if (data.hits.length === 12) {
+//         this.setState({ hasMoreImages: true });
+//       } else {
+//         this.setState({ hasMoreImages: false });
+//       }
+//     } catch (error) {
+//       this.setState({
+//         error,
+//         // status: 'rejected',
+//         hasMoreImages: false,
+//       });
+//     } finally {
+//       this.setState({
+//         isLoading: false,
+//       });
+//     }
+//   };
+
+//   handleSearch = query => {
+//     this.setState(
+//       {
+//         page: 1,
+//         images: [],
+//         query,
+//         hasMoreImages: true,
+//         error: false,
+//       },
+//       () => this.fetchImages(query, 1)
+//     );
+//   };
+
+//   openModal = imageURL => {
+//     this.setState({
+//       showModal: true,
+//       modalImageURL: imageURL,
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({
+//       showModal: false,
+//       modalImageURL: '',
+//     });
+//   };
+
+//   onLoadMore = () => {
+//     this.fetchImages(this.state.query, this.state.page + 1);
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//       error: false,
+//     }));
+//   };
+
+//   render() {
+//     const { error, hasMoreImages } = this.state;
+//     // if (this.state.status === 'pending') return <Loader />;
+//     // else if (this.state.status === 'resolved')
+//     return (
+//       <div className={css.App}>
+//         <Searchbar handleSearch={this.handleSearch} />
+//         <ImageGallery images={this.state.images} openModal={this.openModal} />
+//         {hasMoreImages && <Button onClick={this.onLoadMore} />}
+//         {this.state.showModal && (
+//           <Modal
+//             closeModal={this.closeModal}
+//             largeImageURL={this.state.modalImageURL}
+//           />
+//         )}
+//         {this.state.isLoading && <Loader />}
+//         <ErrorCard>{error}</ErrorCard>
+//       </div>
+//     );
+//     // else if (this.state.status === 'rejected')
+//     // return ;
+//     // else
+//     //   return (
+//     //     <div>
+//     //       {error && <ErrorCard>{error}</ErrorCard>}
+//     //       <div className={css.App}>
+//     //         <Searchbar handleSearch={this.handleSearch} />
+//     //         <ImageGallery
+//     //           images={this.state.images}
+//     //           openModal={this.openModal}
+//     //         />
+//     //         {hasMoreImages && <Button onClick={this.onLoadMore} />}
+//     //         {this.state.showModal && (
+//     //           <Modal
+//     //             closeModal={this.closeModal}
+//     //             largeImageURL={this.state.modalImageURL}
+//     //           />
+//     //         )}
+//     //       </div>
+//     //     </div>
+//     // );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
-import axios from 'axios';
 import css from 'App.module.css';
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
@@ -7,10 +164,7 @@ import Loader from './components/Loader/Loader';
 import Modal from './components/Modal/Modal';
 import Button from './components/Button/Button';
 import ErrorCard from './components/ErrorCard';
-// import fetchImages from './services/fetchImages';
-
-const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+import { fetchImages } from './services/fetchImages';
 
 class App extends Component {
   state = {
@@ -20,55 +174,42 @@ class App extends Component {
     showModal: false,
     query: '',
     page: 1,
-    status: 'idle',
-    units: null,
-    hasMoreImages: true,
-    // hits: 1,
+    hasMoreImages: true, // Initialize it as true
   };
 
-  fetchImages = async (query, page) => {
-    try {
-      this.setState({
-        isLoading: true,
-      });
-      const { data } = await axios.get(
-        `/?q=${query}&key=${API_KEY}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-
-      if (data.hits.length === 0) {
-        this.setState({
-          error: `No results found for "${query}"`,
-          status: 'rejected',
-          hasMoreImages: false,
-        });
-        return;
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.page !== prevState.page ||
+      this.state.query !== prevState.query
+    ) {
+      if (this.state.page !== 1) {
+        this.fetchImagesInternal(this.state.query, this.state.page);
       }
+    }
+  }
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...data.hits],
-        error: null,
-      }));
+  fetchImagesInternal = async (query, page) => {
+    this.setState({
+      isLoading: true,
+    });
 
-      // this.steState(prevState => ({
-      //   images: [...prevState.images, ...data.hits],
-      //   loadMore: this.state.page < Math.ceil(data.totalHits / 12),
-      // }));
+    const { images, error } = await fetchImages(query, page);
 
-      // if (data.status === 'ok') this.setState({ units: data.unit });
-      // else return Promise.reject(data.message);
-
-      if (data.hits.length === 12) {
-        this.setState({ hasMoreImages: true });
-      } else {
-        this.setState({ hasMoreImages: false });
-      }
-    } catch (error) {
-      this.setState({ error, status: 'rejected', hasMoreImages: false });
-    } finally {
+    if (error) {
       this.setState({
+        error,
+        hasMoreImages: false,
         isLoading: false,
       });
+      return;
     }
+
+    this.setState(prevState => ({
+      images: [...prevState.images, ...images],
+      error: null,
+      isLoading: false,
+      hasMoreImages: images.length === 12,
+    }));
   };
 
   handleSearch = query => {
@@ -78,8 +219,9 @@ class App extends Component {
         images: [],
         query,
         hasMoreImages: true,
+        error: '',
       },
-      () => this.fetchImages(query, 1)
+      () => this.fetchImagesInternal(query, 1)
     );
   };
 
@@ -98,51 +240,30 @@ class App extends Component {
   };
 
   onLoadMore = () => {
-    this.fetchImages(this.state.query, this.state.page + 1);
     this.setState(prevState => ({
       page: prevState.page + 1,
+      error: '',
     }));
   };
 
   render() {
-    const { error, hasMoreImages } = this.state;
-    if (this.state.status === 'pending') return <Loader />;
-    else if (this.state.status === 'resolved')
-      return (
-        <div>
-          <Searchbar handleSearch={this.handleSearch} />
-          <ImageGallery images={this.state.images} openModal={this.openModal} />
-          {hasMoreImages && <Button onClick={this.onLoadMore} />}
-          {this.state.showModal && (
-            <Modal
-              closeModal={this.closeModal}
-              largeImageURL={this.state.modalImageURL}
-            />
-          )}
-        </div>
-      );
-    else if (this.state.status === 'rejected')
-      return <ErrorCard>{error}</ErrorCard>;
-    else
-      return (
-        <div>
-          {error && <ErrorCard>{error}</ErrorCard>}
-          <div className={css.App}>
-            <Searchbar handleSearch={this.handleSearch} />
-            <ImageGallery
-              images={this.state.images}
-              openModal={this.openModal}
-            />
-            {hasMoreImages && <Button onClick={this.onLoadMore} />}
-            {this.state.showModal && (
-              <Modal
-                closeModal={this.closeModal}
-                largeImageURL={this.state.modalImageURL}
-              />
-            )}
-          </div>
-        </div>
-      );
+    const { error, hasMoreImages, query } = this.state;
+
+    return (
+      <div className={css.App}>
+        <Searchbar handleSearch={this.handleSearch} />
+        <ImageGallery images={this.state.images} openModal={this.openModal} />
+        {query && hasMoreImages && <Button onClick={this.onLoadMore} />}
+        {this.state.showModal && (
+          <Modal
+            closeModal={this.closeModal}
+            largeImageURL={this.state.modalImageURL}
+          />
+        )}
+        {this.state.isLoading && <Loader />}
+        {error && <ErrorCard>{error}</ErrorCard>}
+      </div>
+    );
   }
 }
 

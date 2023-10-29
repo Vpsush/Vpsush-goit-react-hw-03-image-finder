03c24983d26538740a04ee1470d3a1c5aaa781ba
Vpsush <vpsush2023@gmail.com>
2023-10-29 14:42:15 +0200

    ,,,

--- src/App.jsx
+++ src/App.jsx
@@ -1,234 +1,3 @@
-// import { Component } from 'react';
-// import axios from 'axios';
-// import Searchbar from './Searchbar/Searchbar';
-// import ImageGallery from './ImageGallery/ImageGallery';
-// // import Modal from './ModalModal';
-// import Loader from './Loader/Loader';
-// // import Searchbar from './Searchbar/Searchbar';
-
-// const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
-// axios.defaults.BASE_URL = 'https://pixabay.com/api/';
-
-// export class App extends Component {
-//   state = {
-//     // images: [],
-//     isOpenModal: false,
-//     modalData: null,
-//     imageKey: '',
-//     isLoading: false,
-//     error: null,
-//   };
-
-//   fetchImages = async ({ query, page }) => {
-//     try {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       const { data } = await axios.get(
-//         `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//       );
-
-//       this.setState({
-//         posts: data,
-//       });
-//       return data;
-//     } catch (error) {
-//       this.setState({ error: error.message });
-//     } finally {
-//       this.setState({
-//         isLoading: false,
-//       });
-//     }
-//   };
-
-//   openModal = someDataToModal => {
-//     this.setState({
-//       isOpenModal: true,
-//       modalData: someDataToModal,
-//     });
-//   };
-
-//   closeModal = () => {
-//     this.setState({
-//       isOpenModal: false,
-//       modalData: null,
-//     });
-//   };
-
-//   handleSearch = imageKey => {
-//     this.setState({ imageKey });
-//   };
-
-//   componentDidMount() {
-//     this.fetchImages();
-//   }
-
-//   render() {
-//     // const { images } = this.state;
-//     return (
-//       <div>
-//         {this.state.error !== null && (
-//           <p className="error-bage">
-//             Oops, some error done... Error message: {this.state.error}
-//           </p>
-//         )}
-//         <div>{this.state.isLoading && <Loader />}</div>
-//         <div>
-//           <ImageGallery
-//             imageKey={this.state.imageKey}
-//             // openModal={this.openModal}
-//           />
-//           <Searchbar handleSearch={this.handleSearch} />
-//           {/* <Modal
-//           largeImageURL={largeImageURL}
-//             alt={tags}
-//         // closeModal={this.closeModal}
-//         // modalData={this.state.modalData}
-//         /> */}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// import { Component } from 'react';
-// import axios from 'axios';
-// import css from 'App.module.css';
-// import Searchbar from './components/Searchbar/Searchbar';
-// import ImageGallery from './components/ImageGallery/ImageGallery';
-// import Loader from './components/Loader/Loader';
-// import Modal from './components/Modal/Modal';
-// import Button from './components/Button/Button';
-// import ErrorCard from './components/ErrorCard';
-
-// const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-// class App extends Component {
-//   state = {
-//     images: [],
-//     isLoading: false,
-//     error: '',
-//     showModal: false,
-//     query: '',
-//     // buttonShow: false,
-//     page: 1,
-//     status: 'idle',
-//     units: null,
-//   };
-
-//   // componentDidUpdate(_, prevState) {
-//   //   const prevQuery = prevState.query;
-//   //   const currentQuery = this.state.query;
-//   //   const prevPage = prevState.page;
-//   //   const currentPage = this.state.page;
-
-//   //   if (prevQuery !== this.currentQuery) {
-//   //     this.setState({
-//   //       buttonShow: false,
-//   //       page: 1,
-//   //       images: [],
-//   //     });
-//   //     if (currentPage === 1) {
-//   //       this.fetchImages(currentQuery, currentPage);
-//   //     }
-//   //   } else if (prevPage !== this.currentPage) {
-//   //     this.fetchImages(currentQuery, currentPage);
-//   //   }
-//   // }
-
-//   fetchImages = async query => {
-//     try {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       const { data } = await axios.get(
-//         `/?q=${query}&key=${API_KEY}&page=1&image_type=photo&orientation=horizontal&per_page=12`
-//       );
-
-//       this.setState({
-//         images: data.hits,
-//         error: null,
-//       });
-
-//       if (data.status === 'ok') this.setState({ units: data.unit });
-//       else return Promise.reject(data.message);
-//     } catch (error) {
-//       this.setState({ error, status: 'rejected' });
-//     } finally {
-//       this.setState({
-//         isLoading: false,
-//       });
-//     }
-//   };
-
-//   handleSearch = query => {
-//     this.fetchImages(query);
-//   };
-
-//   openModal = imageURL => {
-//     this.setState({
-//       showModal: true,
-//       modalImageURL: imageURL,
-//     });
-//   };
-
-//   closeModal = () => {
-//     this.setState({
-//       showModal: false,
-//       modalImageURL: '',
-//       // buttonShow: true,
-//     });
-//   };
-
-//   onLoadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { error } = this.state;
-//     if (this.state.status === 'pending') return <Loader />;
-//     else if (this.state.status === 'resolved')
-//       return (
-//         <ImageGallery
-//           images={this.state.images}
-//           openModal={this.openModal}
-//           // galleryItems={galleryItems}
-//         />
-//       );
-//     else if (this.state.status === 'rejected')
-//       return <ErrorCard>{error}</ErrorCard>;
-
-//     return (
-//       <div>
-//         {/* {this.state.error !== '' && (
-//           <p className={css.errorBadge}>
-//             Oops, some error occurred. Error message: {this.state.error}
-//           </p>
-//         )} */}
-//         {this.state.error !== '' && <ErrorCard>{this.state.error}</ErrorCard>}
-//         <div className={css.App}>
-//           {/* {this.state.isLoading && <Loader />} */}
-//           <Searchbar handleSearch={this.handleSearch} />
-//           <ImageGallery images={this.state.images} openModal={this.openModal} />
-//           {/* {this.state.buttonShow && <Button onClick={this.fetchImages} />} */}
-//           <Button onClick={this.onLoadMore} />
-//           {this.state.showModal && (
-//             <Modal
-//               closeModal={this.closeModal}
-//               largeImageURL={this.state.modalImageURL}
-//             />
-//           )}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import axios from 'axios';
 import css from 'App.module.css';
@@ -238,6 +7,7 @@ import Loader from './components/Loader/Loader';
 import Modal from './components/Modal/Modal';
 import Button from './components/Button/Button';
 import ErrorCard from './components/ErrorCard';
+// import fetchImages from './services/fetchImages';
 
 const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
@@ -253,6 +23,7 @@ class App extends Component {
     status: 'idle',
     units: null,
     hasMoreImages: true,
+    // hits: 1,
   };
 
   fetchImages = async (query, page) => {
@@ -278,8 +49,13 @@ class App extends Component {
         error: null,
       }));
 
-      if (data.status === 'ok') this.setState({ units: data.unit });
-      else return Promise.reject(data.message);
+      // this.steState(prevState => ({
+      //   images: [...prevState.images, ...data.hits],
+      //   loadMore: this.state.page < Math.ceil(data.totalHits / 12),
+      // }));
+
+      // if (data.status === 'ok') this.setState({ units: data.unit });
+      // else return Promise.reject(data.message);
 
       if (data.hits.length === 12) {
         this.setState({ hasMoreImages: true });
--- src/services/fetchImages.js
+++ src/services/fetchImages.js
@@ -1,22 +1,38 @@
 // import axios from 'axios';
-// const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
-// axios.defaults.BASE_URL = 'https://pixabay.com/api/';
 
-// const fetchImages = async ({ query, page }) => {
+// const API_KEY = '39094662-f0479bb8b89274a4b188f6f08';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// fetchImages = async (query, page) => {
 //   try {
 //     this.setState({
 //       isLoading: true,
 //     });
-//     const { data } = await axios.get(
-//       `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//     );
 
-//     this.setState({
-//       posts: data,
-//     });
-//     return data;
+//     if (data.hits.length === 0) {
+//       this.setState({
+//         error: `No results found for "${query}"`,
+//         status: 'rejected',
+//         hasMoreImages: false,
+//       });
+//       return;
+//     }
+
+//     this.setState(prevState => ({
+//       images: [...prevState.images, ...data.hits],
+//       error: null,
+//     }));
+
+//     if (data.status === 'ok') this.setState({ units: data.unit });
+//     else return Promise.reject(data.message);
+
+//     if (data.hits.length === 12) {
+//       this.setState({ hasMoreImages: true });
+//     } else {
+//       this.setState({ hasMoreImages: false });
+//     }
 //   } catch (error) {
-//     this.setState({ error: error.message });
+//     this.setState({ error, status: 'rejected', hasMoreImages: false });
 //   } finally {
 //     this.setState({
 //       isLoading: false,
@@ -25,68 +41,3 @@
 // };
 
 // export default fetchImages;
-
-// export default class apiImages {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//   }
-
-//   async fetchImages() {
-//     const params = {
-//       key: 'API_KEY',
-//       q: this.searchQuery,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//       per_page: 12,
-//       page: this.page,
-//     };
-//     try {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       const { data } = await axios.get(`${BASE_URL}?${params.toStrings()}`);
-
-//       this.setState({
-//         posts: data,
-//       });
-//       return data;
-//     } catch (error) {
-//       this.setState({ error: error.message });
-//     } finally {
-//       this.setState({
-//         isLoading: false,
-//       });
-//     }
-//   }
-//   resetPage() {
-//     this.page = 1;
-//   }
-
-//   get query() {
-//     return this.searchQuery;
-//   }
-
-//   getPage() {
-//     return this.page;
-//   }
-
-//   set query(newQuery) {
-//     this.searchQuery = newQuery;
-//   }
-// }
-
-// export const getImage = imageKey => {
-//   return fetch(
-//     `${BASE_URL}?q=${imageKey}&page=1&image_type=photo&orientation=horizontal&per_page=12`,
-//     {
-//       headers: {
-//         key: API_KEY,
-//       },
-//     }
-//   )
-//     .then(resp => resp.json)
-
-//     .then(img => console.log(img));
-// };
